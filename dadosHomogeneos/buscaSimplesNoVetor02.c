a34be8ce63dd75f67e1531e289abdaeb9b5bf181
Vinicius Machado Fraga Matos <viniciusmachadofragamatos8@gmail.com>
2026-04-24 23:34:02 -0300

    Refactor buscaSimplesNoVetor02.c for clarity and fixes

--- dadosHomogeneos/buscaSimplesNoVetor02.c
+++ dadosHomogeneos/buscaSimplesNoVetor02.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main() {
     int v[10];
-    for(int i = 0; i < 10; i++){
+    int z[10];
+    int x, y = 0;
+
+    for(int i = 0; i < 10; i++) {
         scanf("%d", &v[i]);
     }
-    int x;
+
     scanf("%d", &x);
 
-    int y = 0;
-    int z[10];
-    for(int i = 0; i < 10; i++){
-        if(v[i] == x){
-            z[i] = i;
-            y = y + 1 ;
-            
+    for(int i = 0; i < 10; i++) {
+        if(v[i] == x) {
+            z[y] = i;
+            y++;
         }
     }
-    if(y == 0){
+
+    if(y == 0) {
         printf("Mia x");
-    } else{
-        printf("%d\n",y);
-        for(int i = 0; i< 10; i++){
-            printf("%d\n", z[i]);
+    } else {
+        printf("%d\n", y);
+
+        for(int i = 0; i < y; i++) {
+            printf("%d", z[i]);
+
+            if(i < y - 1) {
+                printf(" ");
+            }
+        }
     }
 
     return 0;
